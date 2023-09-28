95e1b640f71cd88235316a04bf4832f15ab6575a
y28lyn <i.beirade@gmail.com>
2023-09-28 18:11:06 +0200

    test

--- src/components/header/AccueilBanner.tsx
+++ src/components/header/AccueilBanner.tsx
@@ -1,7 +1,7 @@
 const AccueilBanner = () => {
   return (
     <div className="text-white">
-      <div className="bg-cover bg-center min-h-screen flex items-center overflow-hidden">
+      <div className="bg-cover bg-center min-h-screen flex items-center overflow-hidden relative">
         <div className="absolute inset-0">
           <img
             loading="lazy"
@@ -17,7 +17,7 @@ const AccueilBanner = () => {
 
         <div className="absolute inset-0 block bg-black/60 md:hidden"></div>
 
-        <div className="container mx-auto text-center z-50">
+        <div className="container mx-auto text-center z-50 relative">
           <h1 className="text-5xl font-extrabold mb-4">Autoclean</h1>
           <p className="text-lg opacity-80 mb-8">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
