620037d2b0d34d7d5f27eb353e5fd80989c49dd5
y28lyn <i.beirade@gmail.com>
2023-10-04 19:47:20 +0200

    emailjs added on the contact form

--- src/components/article/ContactForm.tsx
+++ src/components/article/ContactForm.tsx
@@ -1,4 +1,53 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import validator from "validator";
+
 const ContactForm = () => {
+  const form = useRef<HTMLFormElement | null>(null);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      const userNameInput = form.current.user_name;
+      const userEmailInput = form.current.user_email;
+      const messageInput = form.current.message;
+
+      if (!userNameInput.value.trim()) {
+        setErrorMessage("Veuillez entrer votre nom et prénom.");
+        return;
+      }
+
+      if (!validator.isEmail(userEmailInput.value)) {
+        setErrorMessage("Veuillez entrer une adresse email valide.");
+        return;
+      }
+
+      if (!messageInput.value.trim()) {
+        setErrorMessage("Veuillez entrer votre message.");
+        return;
+      }
+
+      try {
+        const result = await emailjs.sendForm(
+          "service_fpzfpxd",
+          "template_unqnxgs",
+          form.current,
+          "Pp6n1YIzECCp0vtuB"
+        );
+        console.log(result.text);
+        setIsSubmitted(true); // Affiche la boîte de dialogue de confirmation
+        form.current.reset(); // Réinitialise le formulaire
+        setErrorMessage(null); // Réinitialise le message d'erreur
+      } catch (error) {
+        console.error(error);
+        setErrorMessage("Une erreur est survenue lors de l'envoi de l'e-mail.");
+      }
+    }
+  };
+
   return (
     <>
       <section className="h-fit py-10 text-white sm:py-16 lg:py-24">
@@ -40,7 +89,13 @@ const ContactForm = () => {
                     répondre à toutes vos questions ou commentaires.
                   </p>
 
-                  <form action="#" method="POST" className="mt-4">
+                  <form
+                    action="#"
+                    ref={form}
+                    onSubmit={sendEmail}
+                    method="POST"
+                    className="mt-4"
+                  >
                     <div className="space-y-6">
                       <div>
                         <label className="text-base font-medium text-gray-900">
@@ -49,7 +104,7 @@ const ContactForm = () => {
                         <div className="mt-2.5 relative">
                           <input
                             type="text"
-                            name=""
+                            name="user_name"
                             id=""
                             placeholder="Entrez votre nom et prénom"
                             className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 caret-blue-500"
@@ -64,7 +119,7 @@ const ContactForm = () => {
                         <div className="mt-2.5 relative">
                           <input
                             type="text"
-                            name=""
+                            name="user_email"
                             id=""
                             placeholder="Entrez votre e-mail"
                             className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 caret-blue-500"
@@ -78,7 +133,7 @@ const ContactForm = () => {
                         </label>
                         <div className="mt-2.5 relative">
                           <textarea
-                            name=""
+                            name="message"
                             id=""
                             placeholder="Entrez votre message"
                             className="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:ring-blue-500 focus:border-blue-500 caret-blue-500"
@@ -96,6 +151,14 @@ const ContactForm = () => {
                       </div>
                     </div>
                   </form>
+                  {isSubmitted && (
+                    <div className="mt-4 text-green-500">
+                      Votre e-mail a été envoyé avec succès !
+                    </div>
+                  )}
+                  {errorMessage && (
+                    <div className="mt-4 text-red-500">{errorMessage}</div>
+                  )}
                 </div>
               </div>
             </div>
