2f20f2b7a8e22819825ca4c31e70e137e01b49f7
y28lyn <i.beirade@gmail.com>
2023-10-05 10:14:52 +0200

    error in console fixed

--- src/components/article/AccueilArticle.tsx
+++ src/components/article/AccueilArticle.tsx
@@ -20,7 +20,7 @@ const AccueilArticle = () => {
               </h3>
               <ul className="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4">
                 <li className="flex items-center space-x-3">
-                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-autoclean_blue rounded-full">
                     <svg
                       className="w-3.5 h-3.5 text-white"
                       xmlns="http://www.w3.org/2000/svg"
@@ -28,9 +28,9 @@ const AccueilArticle = () => {
                       fill="currentColor"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       ></path>
                     </svg>
                   </div>
@@ -39,7 +39,7 @@ const AccueilArticle = () => {
                   </span>
                 </li>
                 <li className="flex items-center space-x-3">
-                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-autoclean_blue rounded-full">
                     <svg
                       className="w-3.5 h-3.5 text-white"
                       xmlns="http://www.w3.org/2000/svg"
@@ -47,9 +47,9 @@ const AccueilArticle = () => {
                       fill="currentColor"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       ></path>
                     </svg>
                   </div>
@@ -58,7 +58,7 @@ const AccueilArticle = () => {
                   </span>
                 </li>
                 <li className="flex items-center space-x-3">
-                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-autoclean_blue rounded-full">
                     <svg
                       className="w-3.5 h-3.5 text-white"
                       xmlns="http://www.w3.org/2000/svg"
@@ -66,9 +66,9 @@ const AccueilArticle = () => {
                       fill="currentColor"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       ></path>
                     </svg>
                   </div>
@@ -77,7 +77,7 @@ const AccueilArticle = () => {
                   </span>
                 </li>
                 <li className="flex items-center space-x-3">
-                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-autoclean_blue rounded-full">
                     <svg
                       className="w-3.5 h-3.5 text-white"
                       xmlns="http://www.w3.org/2000/svg"
@@ -85,9 +85,9 @@ const AccueilArticle = () => {
                       fill="currentColor"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       ></path>
                     </svg>
                   </div>
