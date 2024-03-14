64618c95cff25d770b9387d12d21d52fc5a1605d
y28lyn <i.beirade@gmail.com>
2024-03-14 17:23:04 +0100

    style: :lipstick: Background / color updated for the futur dark mode

--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -10,7 +10,7 @@ const Accueil = () => {
         <Banner
           title="Autoclean"
           subtitle="L'excellence dans chaque goutte d'eau."
-          backgroundImageUrl="https://images.unsplash.com/photo-1550565076-b2371ea1a324?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          backgroundImageUrl="https://images.unsplash.com/photo-1710246265730-d4f722ec11cc?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           button1Label="Automobile"
           button1Link="/automobile"
           button2Label="Lire plus"
