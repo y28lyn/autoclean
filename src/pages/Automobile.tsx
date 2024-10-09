0c7be8229ab5cd78389cb80de8d5decc1befe195
y28lyn <i.beirade@gmail.com>
2024-10-09 16:40:22 +0200

    Link on automobile fixed

--- src/pages/Automobile.tsx
+++ src/pages/Automobile.tsx
@@ -174,8 +174,8 @@ const Automobile = () => {
           title="Lavage automobile"
           subtitle="Redonnez vie à votre voiture avec notre lavage professionnel."
           backgroundImageUrl="https://images.unsplash.com/photo-1594051673969-172a6f721d3c?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          button1Label="Vitrage"
-          button1Link="/vitrage"
+          button1Label="Textile"
+          button1Link="/textile"
           button2Label="Lire plus"
         />
       </header>
